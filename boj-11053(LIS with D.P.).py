b29ae4f1e5c38c21f11af3b8cc8ca540da234ce8
Yungi <dbsrlskfdk@naver.com>
2021-10-15 01:20:15 +0900

    Create boj-11053(LIS with D.P.).py

---
+++ boj-11053(LIS with D.P.).py
@@ -0,0 +1,11 @@
+N = int(input())
+num_list = list(map(int, input().split(" ")))
+
+dp = [1 for i in range(N)] # 어차피 모든 수는 수열에 자기 자신을 포함하기에, 1이 default 값이 될 것이다.
+# dp : 자신보다 인덱스가 작은 곳부터 자신까지의 LIS
+for i in range(0, N): # 첫번째 수부터 순환을 시작한다.
+    for j in range(0, i): # 
+        if num_list[i] > num_list[j]: # 만약 순환을 돌다가 자신보다 숫자가 작으면, 자신의 dp 보다 해당 숫자의 dp에 1을 더한(num_list[i]의 한 케이스를 늘려주는) 것 중 큰 것을 대입
+            dp[i] = max(dp[i], dp[j]+1)
+            
+print(max(dp))            
