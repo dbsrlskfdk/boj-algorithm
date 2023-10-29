a9997e90e95d31cdc9613e9ee87dd8de290f0b8b
Yungi <dbsrlskfdk@naver.com>
2023-10-29 17:58:09 +0900

    [Gold II] Title: 골목 대장 호석 - 효율성 2, Time: 5656 ms, Memory: 234368 KB, Score: 43 point -BaekjoonHub

--- 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.py
+++ 백준/Gold/20183. 골목 대장 호석 － 효율성 2/골목 대장 호석 － 효율성 2.py
@@ -1,41 +1,50 @@
 from collections import defaultdict
-import heapq, sys
-
-input = sys.stdin.readline
+import heapq
 
 N, M, A, B, C = map(int, input().split())
 graph = defaultdict(list)
+costs = []
 
 for _ in range(M):
     x, y, cost = map(int, input().split())
     graph[x].append((cost, y))
     graph[y].append((cost, x))
+    heapq.heappush(costs, -cost)
     
-heap = []
-visit_list = [[] for _ in range(N + 1)] 
-dist = [float('inf')] * (N + 1)
-heapq.heappush(heap, (0, A))
-dist[A] = 0
-visit_list[A].append((0, 0))
-
-
-while heap:
-    c, v = heapq.heappop(heap)
-    if dist[v] < c:
-        continue
-    for nc, nv in graph[v]:
-        if dist[nv] > c + nc:
-            dist[nv] = c + nc
-            heapq.heappush(visit_list[nv], (c + nc, max(heapq.nsmallest(1, visit_list[v])[0][1], nc)))
-            heapq.heappush(heap, (c + nc, nv))
-            
+L = 0
+R = -heapq.heappop(costs)
 ans = float('inf')
-while visit_list[B]:
-    res = heapq.heappop(visit_list[B])   
-    if res[0] <= C and ans > res[1]:
-        ans = res[1]
+
+def dijkstra(mid):
+    heap = []
+    dist = [float('inf')] * (N + 1)
+    heapq.heappush(heap, (0, A))
+    dist[A] = 0
+    
+    while heap:
+        c, v = heapq.heappop(heap)
+        if dist[v] < c:
+            continue
+        for nc, nv in graph[v]:
+            if nc > mid: # mid보다 큰 cost는 무시
+                continue
+            if dist[nv] > c + nc:
+                dist[nv] = c + nc
+                heapq.heappush(heap, (c + nc, nv))
+                
+                if nv == B and dist[nv] <= C: # B에 도착했을 때 C보다 작은 cost면 True
+                    return True
+    return False
+
+while L <= R:
+    mid = (L + R) // 2
+    if dijkstra(mid):
+        ans = mid
+        R = mid - 1
+    else:
+        L = mid + 1
 
 if ans == float('inf'):
-	print(-1)
+    print(-1)
 else:
-	print(ans)
+    print(ans)
