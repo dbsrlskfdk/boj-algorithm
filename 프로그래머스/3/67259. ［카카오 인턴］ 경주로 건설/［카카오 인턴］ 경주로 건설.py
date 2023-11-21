e187df7acd9b2a3b8afc0656781c70f5dd5f99c9
Yungi <dbsrlskfdk@naver.com>
2023-11-21 21:10:40 +0900

    [level 3] Title: [카카오 인턴] 경주로 건설, Time: 0.14 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
+++ 프로그래머스/3/67259. ［카카오 인턴］ 경주로 건설/［카카오 인턴］ 경주로 건설.py
@@ -1,50 +1,19 @@
 from collections import deque
 
 def solution(board):
-    # dy = [0, 0, -1, 1]
-    # dx = [-1, 1, 0, 0]
-    # n = len(board[0])
-    # price_table = []
-    # for i in range(n):
-    #     price_table.append([float('inf') if j==0 else 1 for j in board[i]])
-    #     
-    # visited = [[False] * n for _ in range(n)]
-    # st = (0, 0, (-1, -1))
-    # price_table[0][0] = 0
-    
     dy = [0, 0, -1, 1]
     dx = [-1, 1, 0, 0]
     n = len(board[0])
     price_table = []
     for i in range(n):
         price_table.append([[float('inf')] * 4 if j==0 else None for j in board[i]])
-    visited = [[False] * n for _ in range(n)]
     st = (0, 0)
     for i in range(4): price_table[0][0][i] = 0 
     
     que = deque([st])
     
-    # while que:
-    #     y, x, prev_move = que.popleft()
-    #     visited[y][x] = True
-    #     # print(y, x)
-    #     for i in range(4):
-    #         ny = y + dy[i]
-    #         nx = x + dx[i]
-    #         if 0 <= ny < n and 0 <= nx < n:
-    #             if board[ny][nx] == 0:
-    #                 if ((prev_move[0] == dy[i]) and (prev_move[1] == dx[i])) or (prev_move[0] == -1 and prev_move[1] == -1):
-    #                     if price_table[ny][nx] >= price_table[y][x] + 100:
-    #                         price_table[ny][nx] = price_table[y][x] + 100
-    #                         que.append((ny, nx, (dy[i], dx[i])))
-    #                 else:
-    #                     if price_table[ny][nx] >= price_table[y][x] + 600:
-    #                         price_table[ny][nx] = price_table[y][x] + 600
-    #                         que.append((ny, nx, (dy[i], dx[i])))
-    #     # pprint(price_table)
     while que:
         y, x = que.popleft()
-        # print(y, x)
         for i in range(4):
             ny = y + dy[i]
             nx = x + dx[i]
@@ -59,8 +28,5 @@ def solution(board):
                         if price_table[ny][nx][i] > price_table[y][x][j] + 600:
                             price_table[ny][nx][i] = price_table[y][x][j] + 600
                             que.append((ny, nx))
-                        
-                    
-                
-        # pprint(price_table)
+                                        
     return min(price_table[n-1][n-1])
