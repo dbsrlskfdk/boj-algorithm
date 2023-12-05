0ca0d2f4732b3c893cc348af114269563f10f249
Yungi <dbsrlskfdk@naver.com>
2023-12-05 13:02:43 +0900

    [Silver I] Title: IOIOI, Time: 344 ms, Memory: 33076 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.py
+++ 백준/Silver/5525. IOIOI/IOIOI.py
@@ -2,11 +2,21 @@ N = int(input())
 M = int(input())
 S = input()
 
-search_word = "IO" * N + "I"
 cnt = 0
+st = 0
+pattern = 0
 
-for i in range(M - (2*N+1) + 1):
-    if search_word == S[i:i+2*N+1]:
-        cnt += 1
-    
+while st < M - 2:
+    if S[st] == "I" and S[st+1] == "O" and S[st+2] == "I":
+        pattern += 1
+        
+        if pattern == N:
+            cnt += 1
+            pattern -= 1
+        
+        st += 1 # 어차피 IOI가 나왔으면, 바로 다음 문자는 볼 필요없이 O니까 
+    else:
+        pattern = 0
+    st += 1
+
 print(cnt)
