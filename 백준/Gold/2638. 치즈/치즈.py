e4b2cef5a44509b58ed5a29146ad4b73fdffa309
Yungi <dbsrlskfdk@naver.com>
2023-12-19 16:42:29 +0900

    [Gold III] Title: 치즈, Time: 372 ms, Memory: 120704 KB -BaekjoonHub

---
+++ 백준/Gold/2638. 치즈/치즈.py
@@ -0,0 +1,70 @@
+from collections import deque
+import sys
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+graphs = [list(map(int, input().split())) for _ in range(N)]
+melt_time = 0
+
+def air_cover_cnt(y, x, N, M):
+    cnt = 0
+    
+    dy = [0, 0, -1, 1]
+    dx = [-1, 1, 0, 0]
+    
+    for i in range(4):
+        ny = y + dy[i]
+        nx = x + dx[i]
+        
+        if 0 <= ny < N and 0 <= nx < M and graphs[ny][nx] == -1:
+            cnt += 1
+            
+    return cnt
+
+
+while True:
+    melt_time += 1
+    
+    que = deque([])
+    dy = [0, 0, -1, 1]
+    dx = [-1, 1, 0 , 0]
+
+    visited = [[False for _ in range(M)] for _ in range(N)]
+    visited[0][0] = True
+    graphs[0][0] = -1
+    que.append([0, 0]) # 방문 큐에 추가
+    
+    while que: # bfs 순회 시작
+        y, x = que.pop()
+
+        for k in range(4):
+            ny = y + dy[k]
+            nx = x + dx[k]
+
+            if 0 <= ny < N and 0 <= nx < M: 
+                if not visited[ny][nx] and (graphs[ny][nx] == 0 or graphs[ny][nx] == -1): # 범위 내에 있으면,
+                    visited[ny][nx] = True
+                    graphs[ny][nx] = -1
+                    que.append([ny, nx])
+      
+    cheese = []
+    visited = [[False for _ in range(M)] for _ in range(N)]
+    for i in range(N):
+        for j in range(M):
+            if graphs[i][j] == 1 and air_cover_cnt(i, j, N, M) > 1:
+                cheese.append([i, j])
+                
+    for i in cheese:
+        graphs[i[0]][i[1]] = -1
+        
+    flag = False
+    for i in range(N):
+        for j in range(M):
+            if graphs[i][j] == 1:
+                flag = True
+                
+    if not flag:
+        break
+
+print(melt_time)
